9afede289c56b7b6927e803f54dc3215b109b58c
congle98 <lethanhcongcnhn@gmail.com>
2021-04-05 09:36:59 +0700

    addfile

--- src/demo.java
+++ src/demo.java
@@ -3,8 +3,11 @@ import java.util.Scanner;
 public class demo {
     static boolean checknguyento(int number){
         boolean check = true;
-        if(number <= 2){
+        if(number <= 1){
             check = false;
+        }
+        else if (number<=3){
+            check = true;
         }
         else {
             for (int i = 2; i <= Math.sqrt(number); i++){
@@ -19,10 +22,10 @@ public class demo {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Mời nhập số lượng số nguyên tố cần in ra");
         int amount = scanner.nextInt();
-        int count = 0;
-        for (int number = 0;count<amount;number++){
+        int count = 1;
+        for (int number = 0;count<=amount;number++){
             if(checknguyento(number)){
-                System.out.println(number);
+                System.out.println("số nguyên tố thứ "+count+" là: " +number);
                 count++;
             }
         }
